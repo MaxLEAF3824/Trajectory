9106eb48b07017edd1f04cb7a6ec2f7b4be45353
MaxLEAF3824 <guoyiqiu_max@foxmail.com>
2022-01-10 20:59:08 +0800

    another change

--- traj2cell.py
+++ traj2cell.py
@@ -1,9 +1,7 @@
 import utils
 from geopy.distance import geodesic as dis
-from math import ceil
 import numpy as np
 # import pandas as pd
-
 
 import modin.pandas as pd
 import ray
@@ -14,7 +12,6 @@ ray.init()
 class Traj2Cell:
 
     def __init__(self, m, n, min_lon, min_lat, max_lon, max_lat):
-        self.cell_count = {}
         self.cell2idx = {}
         self.row_num = m  # 行数
         self.column_num = n  # 列数
@@ -29,22 +26,14 @@ class Traj2Cell:
         p2 = (min_lat, min_lon + (max_lon - min_lon) / n)
         self.cell_shape = (dis(p0, p1).meters, dis(p0, p2).meters)
 
-    def convert2d(self, traj):
+    def convert2d_point(self, point):
         # point : (lon, lat)
-        return [(ceil((p[0] - self.min_lon) / self.l), ceil((p[1] - self.min_lat) / self.h)) for p in traj]
+        return int((point[0] - self.min_lon) // self.l), int((point[1] - self.min_lat) // self.h)
 
-    def load_trajs(self, trajs):
-        # traj = np.array(trajs).reshape(-1, 2).tolist()
-        for cell in self.convert2d(trajs):
-            if not self.cell_count.get(cell):
-                self.cell_count[cell] = 1
-            else:
-                self.cell_count[cell] += 1
-
-    def build_vocab(self, lower_bound=1):
+    def build_vocab(self, cell_count: dict, lower_bound=1):
         self.cell2idx.clear()
-        for idx, cell in enumerate(self.cell_count):
-            if self.cell_count[cell] >= lower_bound:
+        for idx, cell in enumerate(cell_count):
+            if cell_count[cell] >= lower_bound:
                 self.cell2idx[cell] = idx
         return self.cell2idx
 
@@ -53,7 +42,7 @@ class Traj2Cell:
         if not self.cell2idx:
             print("vocab is not built.")
             return []
-        for cell in self.convert2d(traj):
+        for cell in [self.convert2d_point(p) for p in traj]:
             if self.cell2idx.get(cell):
                 traj_1d.append(self.cell2idx[cell])
             else:
@@ -75,15 +64,14 @@ class Traj2Cell:
     def dis_2d(self, c1, c2):
         return abs(c1[0] - c2[0]) + abs(c1[1] - c2[1])
 
-    def draw_cell(self, file_name="cells.png"):
+    def draw_cell(self, cell_count: dict, file_name="cells.png"):
         from PIL import Image
 
         img = Image.new("RGB", (self.row_num, self.column_num))
-        values = np.array(list(self.cell_count.values()))
-        mean = np.mean(values)
-        std = np.std(values)
+        mean = np.mean(list(cell_count.values()))
+        std = np.std(list(cell_count.values()))
         for cell in self.cell2idx:
-            percent = 50 * (self.cell_count[cell] - mean) / std + 50
+            percent = 50 * (cell_count[cell] - mean) / std + 50
             if percent < 50:
                 green = 255
                 red = percent * 5.12
@@ -102,19 +90,25 @@ if __name__ == "__main__":
     timer = utils.Timer()
     t2c = Traj2Cell(row_num, column_num, min_lon, min_lat, max_lon, max_lat)
     print(t2c.cell_shape)
-    timer.tik("")
+    timer.tik()
+    value_counts = None
     for i in range(1, 31):
         str_i = str(i).zfill(2)
         print(str_i)
-        df = pd.read_csv(f"data/full/gps_201611{str_i}", header=None)
-        df.columns = ['name', 'order_id', 'time', 'lon', 'lat']  # lon经度 lat纬度
+        df = pd.read_csv(f"data/full/gps_201611{str_i}", names=['name', 'order_id', 'time', 'lon', 'lat'],
+                         usecols=['lon', 'lat'])  # lon经度 lat纬度
         timer.tok("read")
-        trajs = df[['lon', 'lat']].values.tolist()
-        timer.tok("tolist")
-        t2c.load_trajs(trajs)
-        timer.tok("load")
-    cell2idx = t2c.build_vocab()
-    str_cell2idx = {"({},{})".format(cell[0], cell[1]): idx for idx, cell in enumerate(list(cell2idx))}
+        df = df.apply(t2c.convert2d_point, axis=1).squeeze()
+        timer.tok("apply")
+        if value_counts:
+            value_counts = value_counts.add(df.value_counts(), fill_value=0)
+        else:
+            value_counts = df.value_counts()
+        timer.tok("value_counts")
+    value_counts = value_counts.to_dict()
+    cell2idx = t2c.build_vocab(value_counts)
+    timer.tok("build_vocab")
+    str_cell2idx = {f"({cell[0]},{cell[1]})": cell2idx[cell] for cell in cell2idx}
     import json
 
     js = json.dumps(str_cell2idx)
@@ -123,5 +117,7 @@ if __name__ == "__main__":
         f.close()
 
     for i in range(1, 1001):
-        cell2idx = t2c.build_vocab(i)
-        t2c.draw_cell(f"cells_png_400/cells_lb{i}.png")
+        pic_name = f"cells_png_400/cells_lb{i}.png"
+        cell2idx = t2c.build_vocab(value_counts, i)
+        t2c.draw_cell(value_counts, pic_name)
+        print(pic_name)
