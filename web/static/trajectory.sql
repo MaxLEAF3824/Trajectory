4464283a6b87027dc566994e1f8c5249484047a0
MaxLEAF3824 <guoyiqiu_max@foxmail.com>
2022-03-20 15:47:36 +0800

    web almost done

--- web/static/trajectory.sql
+++ web/static/trajectory.sql
@@ -11,11 +11,23 @@
  Target Server Version : 50651
  File Encoding         : 65001
 
- Date: 19/03/2022 14:16:45
+ Date: 20/03/2022 15:47:27
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for original_trajectory
+-- ----------------------------
+DROP TABLE IF EXISTS `original_trajectory`;
+CREATE TABLE `original_trajectory`  (
+  `id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `time` int(11) NOT NULL,
+  `longitude` float NOT NULL,
+  `latitude` float NOT NULL,
+  PRIMARY KEY (`id`, `time`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for trajectory
@@ -24,8 +36,9 @@ DROP TABLE IF EXISTS `trajectory`;
 CREATE TABLE `trajectory`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `length` int(11) NOT NULL,
-  `points` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `discrete_points` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `spherical_points` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 5451 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
